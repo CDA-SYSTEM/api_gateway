011f73c5ad9eb3aaf60b10057fb570cf5d6c8ff6
Ilesandres <ilesandres6@gmail.com>
2026-05-10 14:47:06 -0500

    :sparkles: feat(reception): import VehicleModule for vehicle data enrichment

--- src/reception/reception.module.ts
+++ src/reception/reception.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ClientsModule } from '../clients/clients.module';
+import { VehicleModule } from '../vehicle/vehicle.module';
 import { ReceptionController } from './reception.controller';
 import { ReceptionService } from './application/reception.service';
 import { ReceptionInfrastructureService } from './infrastructure/reception.service';
 
 @Module({
-  imports: [HttpModule, ConfigModule, ClientsModule],
+  imports: [HttpModule, ConfigModule, ClientsModule, VehicleModule],
   controllers: [ReceptionController],
   providers: [ReceptionService, ReceptionInfrastructureService],
   exports: [ReceptionService, ReceptionInfrastructureService],
