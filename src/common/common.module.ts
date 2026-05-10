1c90cf591fa3eae46d712473e0be8a55f7d7c521
Ilesandres <ilesandres6@gmail.com>
2026-05-10 15:35:52 -0500

    :sparkles: feat(catalogs): add CatalogsModule and register in CommonModule

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -4,10 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { TokenValidationService } from './services/token-validation.service';
 import { UploadFilesModule } from '../upload-files/upload-files.module';
 import { ReceptionModule } from '../reception/reception.module';
+import { CatalogsModule } from '../catalogs/catalogs.module';
 
 @Global()
 @Module({
-  imports: [HttpModule, ConfigModule, UploadFilesModule, ReceptionModule],
+  imports: [HttpModule, ConfigModule, UploadFilesModule, ReceptionModule, CatalogsModule],
   providers: [TokenValidationService],
   exports: [HttpModule, ConfigModule, TokenValidationService, UploadFilesModule, ReceptionModule],
 })
