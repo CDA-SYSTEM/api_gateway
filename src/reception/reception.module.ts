3dee2381d92ca518ee8964998bfee8be24954733
Ilesandres <ilesandres6@gmail.com>
2026-05-10 15:37:15 -0500

    :sparkles: feat(reception): import UploadFilesModule for file upload support

--- src/reception/reception.module.ts
+++ src/reception/reception.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { ClientsModule } from '../clients/clients.module';
 import { VehicleModule } from '../vehicle/vehicle.module';
 import { AuthModule } from '../auth/auth.module';
+import { UploadFilesModule } from '../upload-files/upload-files.module';
 import { ReceptionController } from './reception.controller';
 import { ReceptionService } from './application/reception.service';
 import { ReceptionInfrastructureService } from './infrastructure/reception.service';
 
 @Module({
-  imports: [HttpModule, ConfigModule, ClientsModule, VehicleModule, AuthModule],
+  imports: [HttpModule, ConfigModule, ClientsModule, VehicleModule, AuthModule, UploadFilesModule],
   controllers: [ReceptionController],
   providers: [ReceptionService, ReceptionInfrastructureService],
   exports: [ReceptionService, ReceptionInfrastructureService],
