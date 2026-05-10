b2892cd3dbe72529f0f7b27ba33a17da54d86783
Ilesandres <ilesandres6@gmail.com>
2026-05-10 10:42:39 -0500

    :sparkles: feat(reception): add infrastructure service with proxyRequest and safeParse

---
+++ src/reception/infrastructure/reception.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { Observable, map, catchError } from 'rxjs';
+import { AxiosResponse, AxiosError } from 'axios';
+import { safeParse } from '../../common/utils/parse.util';
+
+@Injectable()
+export class ReceptionInfrastructureService {
+  private readonly baseUrl: string;
+
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.baseUrl = this.configService.get<string>('RECEPTION_SERVICE_BASE_URL') || '';
+    if (!this.baseUrl) {
+      throw new Error('RECEPTION_SERVICE_BASE_URL is not defined');
+    }
+  }
+
+  proxyRequest(method: string, path: string, data?: any, headers?: any): Observable<any> {
+    const url = `${this.baseUrl}${path}`;
+    return this.httpService.request({ method, url, data, headers }).pipe(
+      map((response: AxiosResponse) => safeParse(response.data)),
+      catchError((error: AxiosError) => {
+        if (error.code === 'ECONNREFUSED' || error.code === 'ENOTFOUND') {
+          throw new HttpException('Reception service no disponible', HttpStatus.BAD_GATEWAY);
+        }
+        throw new HttpException(
+          error.response?.data || `Service error: ${error.message}`,
+          error.response?.status || HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }),
+    );
+  }
+}
