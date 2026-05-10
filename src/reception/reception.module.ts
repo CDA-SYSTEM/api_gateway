360980632b524a56e0657413b7c8b40ac04c7a71
Ilesandres <ilesandres6@gmail.com>
2026-05-10 11:13:54 -0500

    :sparkles: feat(reception): import ClientsModule for client data enrichment

--- src/reception/reception.module.ts
+++ src/reception/reception.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule } from '../clients/clients.module';
 import { ReceptionController } from './reception.controller';
 import { ReceptionService } from './application/reception.service';
 import { ReceptionInfrastructureService } from './infrastructure/reception.service';
 
 @Module({
-  imports: [HttpModule, ConfigModule],
+  imports: [HttpModule, ConfigModule, ClientsModule],
   controllers: [ReceptionController],
   providers: [ReceptionService, ReceptionInfrastructureService],
   exports: [ReceptionService, ReceptionInfrastructureService],
