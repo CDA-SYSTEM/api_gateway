42aba3844598cf1ba9bbed9927109c3a6b18ff1a
Ilesandres <ilesandres6@gmail.com>
2026-05-10 07:41:27 -0500

    :sparkles: feat(upload-files): register UploadFilesService in module providers

--- src/upload-files/upload-files.module.ts
+++ src/upload-files/upload-files.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { UploadFilesController } from './upload-files.controller';
+import { UploadFilesService } from './application/upload-files.service';
 import { UploadFilesInfrastructureService } from './infrastructure/upload-files.service';
 
 @Module({
   imports: [HttpModule, ConfigModule],
   controllers: [UploadFilesController],
-  providers: [UploadFilesInfrastructureService],
-  exports: [UploadFilesInfrastructureService],
+  providers: [UploadFilesService, UploadFilesInfrastructureService],
+  exports: [UploadFilesService, UploadFilesInfrastructureService],
 })
 export class UploadFilesModule {}
