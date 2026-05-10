1d22bbf2013baa205a64db84e1ec24bf6dc952e5
Ilesandres <ilesandres6@gmail.com>
2026-05-10 07:36:21 -0500

    :sparkles: feat(common): register UploadFilesModule in CommonModule as global

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { TokenValidationService } from './services/token-validation.service';
+import { UploadFilesModule } from '../upload-files/upload-files.module';
 
 @Global()
 @Module({
-  imports: [HttpModule, ConfigModule],
+  imports: [HttpModule, ConfigModule, UploadFilesModule],
   providers: [TokenValidationService],
-  exports: [HttpModule, ConfigModule, TokenValidationService],
+  exports: [HttpModule, ConfigModule, TokenValidationService, UploadFilesModule],
 })
 export class CommonModule {}
