feba6cdc7e9adc50bc6dbe5d35c18abea68d8acf
Ilesandres <ilesandres6@gmail.com>
2026-05-10 16:37:17 -0500

    :sparkles: feat(catalogs): wire CatalogsModule with HttpModule and ConfigModule, remove hardcoded catalogs.data.ts
    CatalogsModule now imports HttpModule and ConfigModule, provides CatalogsService and CatalogsInfrastructureService. The hardcoded catalogs.data.ts is no longer needed since all catalog data is proxied from form-services.

--- src/catalogs/catalogs.module.ts
+++ src/catalogs/catalogs.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { CatalogsController } from './catalogs.controller';
+import { CatalogsService } from './application/catalogs.service';
+import { CatalogsInfrastructureService } from './infrastructure/catalogs.service';
 
 @Module({
+  imports: [HttpModule, ConfigModule],
   controllers: [CatalogsController],
+  providers: [CatalogsService, CatalogsInfrastructureService],
 })
 export class CatalogsModule {}
