a12430745ce10a482bc49d12691d944f84b3acaf
Ilesandres <ilesandres6@gmail.com>
2026-05-10 14:55:37 -0500

    :sparkles: feat(reception): add UserData interface

---
+++ src/reception/application/dtos/user-data.interface.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+  id: number;
+  identificationType: string;
+  identificationNumber: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  role: string;
+}
