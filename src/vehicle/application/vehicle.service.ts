b246ed87e970431b5ec4b0f5f4fe7f8fa8fb076b
Ilesandres <ilesandres6@gmail.com>
2026-05-16 08:32:29 -0500

    :sparkles: feat(vehicle): add deleteVehicle method to VehicleService
    Proxies DELETE /vehiculo/{id} to the vehicle microservice.

--- src/vehicle/application/vehicle.service.ts
+++ src/vehicle/application/vehicle.service.ts
@@ -50,6 +50,12 @@ export class VehicleService {
     });
   }
 
+  deleteVehicle(id: string, token: string): Observable<any> {
+    return this.vehicleInfrastructureService.proxyRequest('DELETE', `/vehiculo/${id}`, null, {
+      Authorization: `Bearer ${token}`,
+    });
+  }
+
   // Marcas
   createMarca(data: CreateCatalogoDto, token: string): Observable<any> {
     return this.vehicleInfrastructureService.proxyRequest('POST', '/marca', data, {
