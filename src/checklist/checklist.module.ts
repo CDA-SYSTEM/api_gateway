91fa60971a6754ac46f5b0d2ce3af9db4aafc499
Ilesandres <ilesandres6@gmail.com>
2026-05-23 09:48:24 -0500

    :sparkles: feat(checklist): exportar TemplatesChecklistService y ChecklistInfrastructureService

--- src/checklist/checklist.module.ts
+++ src/checklist/checklist.module.ts
@@ -24,7 +24,7 @@ import { VehicleModule } from '../vehicle/vehicle.module';
     LabradoChecklistService,
     ChecklistInfrastructureService,
   ],
-  exports: [],
+  exports: [TemplatesChecklistService, InspectionsChecklistService, ChecklistInfrastructureService],
 })
 export class ChecklistModule {}
 
